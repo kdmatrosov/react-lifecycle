18a6733ff38c5117701149802b446630478c9be5
Kirill Matrosov <kdmatrosov@gmail.com>
2018-09-01 21:16:41 +0300

    init Mounting step

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Redirect, Route } from "react-router-dom";
+import Mounting from "./routes/Mounting";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route path="/mounting" component={Mounting}/>
+          <Redirect to="/mounting"/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/routes/Mounting.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Mounting extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return null;
+  }
+
+  componentDidMount = () => {
+
+  };
+
+  render() {
+    return null;
+  }
+}
+
+export default Mounting;
